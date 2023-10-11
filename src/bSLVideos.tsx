91286c40b8524ab77d4b5dd54a40085a8ee3638d
Jean Flower <jeanflower@rocketmail.com>
2023-10-11 16:02:22 +0100

    Escape apostrophes

--- src/bSLVideos.tsx
+++ src/bSLVideos.tsx
@@ -5,17 +5,17 @@ export let videos:any[] = [];
 videos.push({
   level: 0, 
   name: 'coffee', 
-  embedCode: <><blockquote className="signbsldata-embed" data-vidref="alydedtkwk"><a href="https://www.signbsl.com/sign/coffee">Watch how to sign 'coffee' in British Sign Language</a></blockquote><script async src="https://embed.signbsl.com/widgets.js"></script></> 
+  embedCode: <><blockquote className="signbsldata-embed" data-vidref="alydedtkwk"><a href="https://www.signbsl.com/sign/coffee">Watch how to sign &apos;coffee&apos; in British Sign Language</a></blockquote><script async src="https://embed.signbsl.com/widgets.js"></script></> 
 });
 videos.push({
   level: 0, 
   name: 'tea', 
-  embedCode: <><blockquote className="signbsldata-embed" data-vidref="zl3yaf85hv"><a href="https://www.signbsl.com/sign/tea">Watch how to sign 'tea' in British Sign Language</a></blockquote><script async src="https://embed.signbsl.com/widgets.js"></script></> 
+  embedCode: <><blockquote className="signbsldata-embed" data-vidref="zl3yaf85hv"><a href="https://www.signbsl.com/sign/tea">Watch how to sign &apos;tea&apos; in British Sign Language</a></blockquote><script async src="https://embed.signbsl.com/widgets.js"></script></> 
 });
 videos.push({
   level: 0, 
   name: 'good morning', 
-  embedCode: <><blockquote className="signbsldata-embed" data-vidref="vyweujxrb6"><a href="https://www.signbsl.com/sign/good-morning">Watch how to sign 'good morning' in British Sign Language</a></blockquote><script async src="https://embed.signbsl.com/widgets.js"></script></>
+  embedCode: <><blockquote className="signbsldata-embed" data-vidref="vyweujxrb6"><a href="https://www.signbsl.com/sign/good-morning">Watch how to sign &apos;good morning&apos; in British Sign Language</a></blockquote><script async src="https://embed.signbsl.com/widgets.js"></script></>
 });
 
 videos.forEach((v) => {
