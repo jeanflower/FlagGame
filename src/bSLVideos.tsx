0c7ea762b783c08f3773615c5476bb2b9974cea7
Jean Flower <jeanflower@rocketmail.com>
2023-10-11 20:48:26 +0100

    Capitalize BSL words

--- src/bSLVideos.tsx
+++ src/bSLVideos.tsx
@@ -4,22 +4,22 @@ export let videos:any[] = [];
 
 videos.push({
   level: 0,
-  name: 'coffee',
+  name: 'Coffee',
   embedCode: `<blockquote class="signbsldata-embed" data-vidref="alydedtkwk"><a href="https://www.signbsl.com/sign/coffee">Watch how to sign 'coffee' in British Sign Language</a></blockquote><script async src="https://embed.signbsl.com/widgets.js" charset="utf-8"></script>`
   });
   videos.push({
   level: 0,
-  name: 'tea',
+  name: 'Tea',
   embedCode: `<blockquote class="signbsldata-embed" data-vidref="zl3yaf85hv"><a href="https://www.signbsl.com/sign/tea">Watch how to sign 'tea' in British Sign Language</a></blockquote><script async src="https://embed.signbsl.com/widgets.js" charset="utf-8"></script>`
   });
   videos.push({
   level: 0,
-  name: 'good morning',
+  name: 'Good morning',
   embedCode: `<blockquote class="signbsldata-embed" data-vidref="vyweujxrb6"><a href="https://www.signbsl.com/sign/good-morning">Watch how to sign 'good morning' in British Sign Language</a></blockquote><script async src="https://embed.signbsl.com/widgets.js" charset="utf-8"></script>`
   });
   videos.push({
   level: 0,
-  name: 'hello',
+  name: 'Hello',
   embedCode: `<blockquote class="signbsldata-embed" data-vidref="cnyia0upyj"><a href="https://www.signbsl.com/sign/hello">Watch how to sign 'hello' in British Sign Language</a></blockquote><script async src="https://embed.signbsl.com/widgets.js" charset="utf-8"></script>`
   });
   videos.push({
