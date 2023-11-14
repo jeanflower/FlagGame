c04831469fb9f34687a260a1c2816b11e7eb39bc
Jean Flower <jeanflower@rocketmail.com>
2023-11-14 23:16:14 +0000

    Add no-routes page

---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { GameApp, gameModes, gameTypes, selectFromFifteen } from '@/App';
+import ClientOnly from '@/ClientOnly';
+import "./../bootstrap/dist/css/bootstrap.css";
+import { useSearchParams } from 'next/navigation';
+
+export default function BSLAphabetGame() {
+
+  const searchParams = useSearchParams();
+  const mode = searchParams.get('mode')
+  const level = searchParams.get('level')
+
+  let defaultMode = selectFromFifteen;
+  let chosenMode = gameModes.find((m) => {
+    return m.name === mode;
+  });
+  // console.log(`chosenMode = ${chosenMode}`);
+  let defaultLevel = 0;
+  let chosenLevel = [0,1,2].find((l) => {
+    return `${l}` === level;
+  });
+
+
+  return (<ClientOnly><GameApp
+    defaultGameMode={
+      chosenMode ? chosenMode.name : defaultMode
+    }
+    defaultGameType={gameTypes.flagGame}
+    defaultGameLevel={
+      chosenLevel ? chosenLevel : defaultLevel
+    }
+  /></ClientOnly>);
+}
