8f73f3cea64290d072350defa00bc935e8832f35
Jean Flower <jeanflower@rocketmail.com>
2025-01-01 15:24:38 +0000

    Full width map

--- src/app/walk/page.tsx
+++ src/app/walk/page.tsx
@@ -4,5 +4,5 @@ import "./../../bootstrap/dist/css/bootstrap.css";
 
 export default function map() {
 
-  return (<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1Evljlj9wptw94mHPH04U32okD-dWzgM&ehbc=2E312F" width="800" height="800"></iframe>);
+  return (<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1Evljlj9wptw94mHPH04U32okD-dWzgM&ehbc=2E312F" width="100%" height="800"></iframe>);
 }
