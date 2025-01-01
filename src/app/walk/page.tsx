9be1553d145b02bfdcee5b73965a51bfd43521a6
Jean Flower <jeanflower@rocketmail.com>
2025-01-01 13:33:33 +0000

    Move map to walk

--- src/app/walk/page.tsx
+++ src/app/walk/page.tsx
@@ -2,7 +2,7 @@
 
 import "./../../bootstrap/dist/css/bootstrap.css";
 
-export default function Map() {
+export default function map() {
 
   return (<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1Evljlj9wptw94mHPH04U32okD-dWzgM&ehbc=2E312F" width="800" height="800"></iframe>);
 }
