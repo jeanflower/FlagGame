5097a433f64b4e4c9c46b118eb1cf0a7b28593ba
Jean Flower <jeanflower@rocketmail.com>
2025-01-01 15:59:10 +0000

    Display updates

--- src/app/walk/page.tsx
+++ src/app/walk/page.tsx
@@ -3,6 +3,32 @@
 import "./../../bootstrap/dist/css/bootstrap.css";
 
 export default function map() {
+  const data = [
+    { date: "1 Jan 2025", steps: 9000, miles: 0.0, combined: 0.0 },
+  ];
+  let grandTotal = 0.0;
+  for(const entry of data) {
+    const combined = (entry.miles + entry.steps / 2000);
+    grandTotal += combined
+    entry.combined = combined;
+  }
 
-  return (<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1Evljlj9wptw94mHPH04U32okD-dWzgM&ehbc=2E312F" width="100%" height="800"></iframe>);
+
+
+  return (<>
+    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1Evljlj9wptw94mHPH04U32okD-dWzgM&ehbc=2E312F" width="100%" height="600"></iframe>
+    <div style={{ width: "100%", padding: "10px" }}>
+        <h2>Progress Updates</h2>
+        <ul>
+          {data.map((entry, index) => {
+            return (
+              <li key={index}>
+                <strong>{entry.date}</strong>: {entry.steps} steps + {entry.miles} miles = {entry.combined.toFixed(1)} miles
+              </li>
+            )}
+          )}
+        </ul>
+        <strong>Total distance so far</strong>: {grandTotal.toFixed(1)} miles
+      </div>
+  </>);
 }
