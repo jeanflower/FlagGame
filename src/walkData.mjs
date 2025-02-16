db94c1a7fd12f861ade585491295fc46b48ec3a5
Jean Flower <jeanflower@rocketmail.com>
2025-02-16 09:47:25 +0530

    14 Feb update

--- src/walkData.mjs
+++ src/walkData.mjs
@@ -1,4 +1,5 @@
 export const walkData = [
+  { date: "14 Feb 2025", combinedKm: 40.00 },
   { date: "13 Feb 2025", combinedKm: 25.00 },
   { date: "12 Feb 2025", combinedKm: 43.88 },
   { date: "11 Feb 2025", combinedKm: 18.92 },
